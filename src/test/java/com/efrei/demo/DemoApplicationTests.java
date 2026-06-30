944a096dc58f4c71c96ad4ac07cc91502fe36741
Dalisha <ngatchedalisha@gmail.com>
2026-06-30 13:10:18 +0200

    Fix code

--- src/test/java/com/efrei/demo/DemoApplicationTests.java
+++ src/test/java/com/efrei/demo/DemoApplicationTests.java
@@ -1,30 +1,13 @@
 package com.efrei.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class DemoApplicationTests {
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
         // Test de chargement du contexte Spring Boot
-    }
-
-    @Test
-    void helloWorldEndpointReturnsOk() throws Exception {
-        mockMvc.perform(get("/"))
-               .andExpect(status().isOk())
-               .andExpect(content().string("Hello, World!"));
     }
 }
